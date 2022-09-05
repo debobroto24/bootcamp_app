72a4b31d78306dbedbf28422a64711d10f5b97ca
dev123-oss <devdb017@gmail.com>
2022-09-05 14:02:33 +0600

    clone bootcamp app

---
+++ lib/controller/blog/blogs_list_controller.dart
@@ -0,0 +1,43 @@
+import 'package:bootcamp_app/controller/base/base_state.dart';
+import 'package:bootcamp_app/controller/blog/state/blog_state.dart';
+import 'package:bootcamp_app/model/blog_model.dart';
+import 'package:bootcamp_app/network/endpoints.dart';
+import 'package:bootcamp_app/network/network_utils.dart';
+import 'package:riverpod/riverpod.dart';
+
+final blogsListProvider = StateNotifierProvider<BlogsListController, BaseState>(
+  (ref) => BlogsListController(ref: ref),
+);
+
+class BlogsListController extends StateNotifier<BaseState> {
+  final Ref? ref;
+  BlogsListController({this.ref}) : super(const InitialState());
+
+  List<BlogModel> blogsList = [];
+
+  Future fetchBlogsList() async {
+    state = const LoadingState();
+    dynamic responseBody;
+
+    try {
+      responseBody = await Network.handleResponse(
+          await Network.getRequest(API.getAllBlogs));
+      //   await Network.getRequest(
+      //       'https://apiv1.techofic.com/app/techofice/get-all-blog-list'),
+      // );
+      if (responseBody != null) {
+        blogsList = (responseBody['data'] as List<dynamic>)
+            .map((x) => BlogModel.fromJson(x))
+            .toList();
+
+        state = BlogsListSuccessState(blogsList);
+      } else {
+        state = const ErrorState();
+      }
+    } catch (error, stackTrace) {
+      print('fetchBlogsList() error = $error');
+      print(stackTrace);
+      state = const ErrorState();
+    }
+  }
+}
